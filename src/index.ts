17dc5d91e7fc938ccdfd6bb36c7b10f31d477213
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2025-06-21 01:40:25 +0200

    Refactor export pattern and add an index file use for main entr

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,12 @@ declare global {
 	interface Window {
 		abimongoUtils: typeof createLogger;
 	}
-}
-
+};
 
 if (typeof window !== 'undefined') {
 	window.abimongoUtils = createLogger;
 	window.Buffer = Buffer;
-}
+};
 
 export * from './logger';
 export * from './transports'
